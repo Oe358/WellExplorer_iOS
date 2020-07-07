44eed787aadbd12b50498a6ea828361fa783e447
owen <owen.wetherbee@gmail.com>
2020-07-07 13:51:33 -0400

    Initial Commit

---
+++ WellExplorerFinal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WellExplorerFinal
+//
+//  Created by Owen Wetherbee on 7/7/20.
+//  Copyright © 2020 Owen Wetherbee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WellExplorerFinal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WellExplorerFinal
+//
+//  Created by Owen Wetherbee on 7/7/20.
+//  Copyright © 2020 Owen Wetherbee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
